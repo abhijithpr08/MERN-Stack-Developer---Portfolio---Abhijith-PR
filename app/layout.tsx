a18b1592a4ea7dffc7d177834f2187f7bcc4ec8c
abhijithpr08 <abhith628@gmail.com>
2026-01-24 14:37:27 +0530

    complete porject setup

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const font= Inter({
+  weight:['100','200','300','400','500','600','700','800','900'],
+  subsets:["latin"]
+})
+
+export const metadata: Metadata = {
+  title: "Protfolio",
+  description: "Portfolio using next.js 16",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${font.className} antialiased bg-[#0d0d1f] `}>
+        {children}
+      </body>
+    </html>
+  );
+}
