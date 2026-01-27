13485facdb6f50e30ef6c6148380df58938de661
abhijithpr08 <abhith628@gmail.com>
2026-01-27 21:39:00 +0530

    add project and skill sections

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from './Hero/Hero'
 import Services from './Services/Services'
 import Resume from './Resume/Resume'
+import Projects from './Projects/Projects'
+import Skills from './Skills/Skills'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero/>
       <Services/>
       <Resume/>
+      <Projects/>
+      <Skills/>
     </div>
   )
 }
