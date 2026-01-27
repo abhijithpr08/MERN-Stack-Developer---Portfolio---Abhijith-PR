76af2b3758ee657485ae73b639e27e80dda1892d
abhijithpr08 <abhith628@gmail.com>
2026-01-27 23:50:56 +0530

    Footer section completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Home/Navbar/Nav";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+import Footer from "@/components/Home/Footer/Footer";
 
 const font= Inter({
   weight:['100','200','300','400','500','600','700','800','900'],
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={`${font.className} antialiased bg-[#0d0d1f] `}>
         <ResponsiveNav/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
