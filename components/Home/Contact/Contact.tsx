763dfbaa0999dff8cc624b516597158b564f7c6b
abhijithpr08 <abhith628@gmail.com>
2026-01-28 10:51:48 +0530

    add emailjs and download cv

--- components/Home/Contact/Contact.tsx
+++ components/Home/Contact/Contact.tsx
@@ -1,70 +1,123 @@
-import React from 'react'
-import { BiEnvelope, BiMap, BiPhone } from 'react-icons/bi'
-import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
+"use client";
+import React from "react";
+import { BiEnvelope, BiMap, BiPhone } from "react-icons/bi";
+import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
 const Contact = () => {
-  return (
-    <div className='pt-16 pb-16'>
-        <div className='w-[90%] md:w-[80%] lg:w-[70%] m-auto grid grid-cols-1 lg:grid-cols-2 gap-10 items-center'>
-            {/* text content */}
-            <div>
-                <h1 className='text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-200'>
-                    Schedule a call with me to see if I can help
-                </h1>
-                <p className='text-gray-400 mt-6 text-base sm:text-lg'>Reach out to me today and let's discuss how I can help you achieve your goals.</p>
-                {/* info */}
-                <div className='mt-7'>
-                    <div className='flex items-center space-x-3 mb-4'>
-                        <BiPhone className='w-9 h-9 text-cyan-300'/>
-                        <p className='text-xl font-bold text-gray-400'>
-                            +91 6282369345
-                        </p>
-                    </div>
-                    <div className='flex items-center space-x-3 mb-4'>
-                        <BiEnvelope className='w-9 h-9 text-cyan-300'/>
-                        <p className='text-xl font-bold text-gray-400'>
-                            Abhi@gmail.com
-                        </p>
-                    </div>
-                    <div className='flex items-center space-x-3 mb-4'>
-                        <BiMap className='w-9 h-9 text-cyan-300'/>
-                        <p className='text-xl font-bold text-gray-400'>
-                            Aluva, Kerala
-                        </p>
-                    </div>
-                </div>
-                {/* social icons */}
-                <div className='flex items-center mt-8 space-x-3'>
-                    <div className='w-14 h-14 bg-blue-950/60 rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-blue-800 transition-all duration-300'>
-                        <FaFacebookF className='text-white w-6 h-6'/>
-                    </div>
-                    <div className='w-14 h-14 bg-blue-950/60 rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-red-600 transition-all duration-300'>
-                        <FaYoutube className='text-white w-6 h-6'/>
-                    </div>
-                    <div className='w-14 h-14 bg-blue-950/60 rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-sky-400 transition-all duration-300'>
-                        <FaTwitter className='text-white w-6 h-6'/>
-                    </div>
-                    <div className='w-14 h-14 bg-blue-950/60 rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-pink-500 transition-all duration-300'>
-                        <FaInstagram className='text-white w-6 h-6'/>
-                    </div>
-                </div>
-            </div>
-            {/* form */}
-            <div data-aos="zoom-in" data-aos-anchor-placement="top-center" data-aos-delay="0" className='md:p-10 p-5 bg-[#131332] rounded-lg'>
-                <input type="text" placeholder='Name' className='px-4 py-3.5 bg-[#363659] text-white outline-none
-                rounded-md w-full placeholder:text:white/70' />
-                <input type="email" placeholder='Email Address' className='px-4 py-3.5 mt-6 bg-[#363659] text-white outline-none
-                rounded-md w-full placeholder:text:white/70' />
-                <input type="text" placeholder='Mobile Number' className='px-4 py-3.5 mt-6 bg-[#363659] text-white outline-none
-                rounded-md w-full placeholder:text:white/70' />
-                <textarea placeholder='Your Message' className='px-4 py-3.5 mt-6 bg-[#363659] text-white outline-none
-                rounded-md w-full placeholder:text:white/70 h-[10rem]'></textarea>
-                <button className='mt-8 px-12 py-4 bg-blue-950 hover:bg-blue-900 transition-all duration-300 cursor-pointer
-                text-white rounded-full'>Send Message</button>
-            </div>
-        </div>
-    </div>
-  )
-}
+  const form = useRef<HTMLFormElement | null>(null);
 
-export default Contact
+  const sendEmail = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!form.current) return;
+
+    emailjs
+      .sendForm(
+        "service_a3jye97",
+        "template_rwot8qq",
+        form.current,
+        "k528Mnhy_2EE5Gvgz",
+      )
+      .then(() => {
+        alert("Message sent successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Failed to send");
+      });
+  };
+
+  return (
+    <div className="pt-16 pb-16">
+      <div className="w-[90%] md:w-[80%] lg:w-[70%] m-auto grid grid-cols-1 lg:grid-cols-2 gap-10 items-center">
+        {/* text content */}
+        <div>
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-200">
+            Schedule a call with me to see if I can help
+          </h1>
+          <p className="text-gray-400 mt-6 text-base sm:text-lg">
+            Reach out to me today and let's discuss how I can help you achieve
+            your goals.
+          </p>
+          {/* info */}
+          <div className="mt-7">
+            <div className="flex items-center space-x-3 mb-4">
+              <BiPhone className="w-9 h-9 text-cyan-300" />
+              <p className="text-xl font-bold text-gray-400">+91 6282369345</p>
+            </div>
+            <div className="flex items-center space-x-3 mb-4">
+              <BiEnvelope className="w-9 h-9 text-cyan-300" />
+              <p className="text-xl font-bold text-gray-400">abhith628@gmail.com</p>
+            </div>
+            <div className="flex items-center space-x-3 mb-4">
+              <BiMap className="w-9 h-9 text-cyan-300" />
+              <p className="text-xl font-bold text-gray-400">Aluva, Kerala</p>
+            </div>
+          </div>
+          {/* social icons */}
+          <div className="flex items-center mt-8 space-x-3">
+            <div className="w-14 h-14 bg-blue-950/60 rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-blue-800 transition-all duration-300">
+              <FaFacebookF className="text-white w-6 h-6" />
+            </div>
+            <div className="w-14 h-14 bg-blue-950/60 rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-red-600 transition-all duration-300">
+              <FaYoutube className="text-white w-6 h-6" />
+            </div>
+            <div className="w-14 h-14 bg-blue-950/60 rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-sky-400 transition-all duration-300">
+              <FaTwitter className="text-white w-6 h-6" />
+            </div>
+            <div className="w-14 h-14 bg-blue-950/60 rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-pink-500 transition-all duration-300">
+              <FaInstagram className="text-white w-6 h-6" />
+            </div>
+          </div>
+        </div>
+        {/* form */}
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          data-aos="zoom-in"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="0"
+          className="md:p-10 p-5 bg-[#131332] rounded-lg"
+        >
+          <input
+            name="from_name"
+            type="text"
+            placeholder="Name"
+            className="px-4 py-3.5 bg-[#363659] text-white outline-none rounded-md w-full"
+          />
+
+          <input
+            name="from_email"
+            type="email"
+            placeholder="Email Address"
+            className="px-4 py-3.5 mt-6 bg-[#363659] text-white outline-none rounded-md w-full"
+          />
+
+          <input
+            name="mobile"
+            type="text"
+            placeholder="Mobile Number"
+            className="px-4 py-3.5 mt-6 bg-[#363659] text-white outline-none rounded-md w-full"
+          />
+
+          <textarea
+            name="message"
+            placeholder="Your Message"
+            className="px-4 py-3.5 mt-6 bg-[#363659] text-white outline-none rounded-md w-full h-[10rem]"
+          ></textarea>
+
+          <button
+            type="submit"
+            className="mt-8 px-12 py-4 bg-blue-950 hover:bg-blue-900 transition-all duration-300 cursor-pointer text-white rounded-full"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
